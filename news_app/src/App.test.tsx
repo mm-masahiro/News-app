d41831ae745f0e115773aac772d1ba2d9f5791f1
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-01-25 23:52:08 +0900

    create top page

--- news_app/src/App.test.tsx
+++ news_app/src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Top from './components/Top'
 
 test('renders learn react link', () => {
   render(<App />);
---
+++ news_app/src/components/Top.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Top() {
+	return (
+		<h1>HogeHoge</h1>
+	)
+}
+
+export default Top;
