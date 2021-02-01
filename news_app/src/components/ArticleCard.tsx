3187fe649c5800763926ff9b8fd32a468d93b283
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-02-01 21:49:11 +0900

    create article card

---
+++ news_app/src/components/ArticleCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/article_card.css"
+
+function ArticleCard() {
+	return (
+		<div className="card">
+			<div className="card--title">Title</div>
+			<div className="card--contents">Contents</div>
+		</div>
+	)
+}
+
+export default ArticleCard;
--- news_app/src/pages/Top.tsx
+++ news_app/src/pages/Top.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import NewsColumn from '../components/NewsColumn';
+import ArticleCard from '../components/ArticleCard';
+// import NewsColumn from '../components/NewsColumn';
 import '../styles/top_page.css'
 
 function Top() {
 	return (
-		<div className="Top">
+		<div className="top">
 			<h1>News Page</h1>
-			<div className="Top--colum">
-				<ul className="Top--colum__list">
-					<li className="Top--colum__list-contents">ニュース</li>
-					<li className="Top--colum__list-contents">マーケット情報</li>
+			<div className="top--colum">
+				<ul className="top--colum__list">
+					<li className="top--colum__list-contents">ニュース</li>
+					<li className="top--colum__list-contents">マーケット情報</li>
 				</ul>
 			</div>
-			<div>
-				<NewsColumn />
+			<div className="top--contents">
+				<ArticleCard />
 			</div>
 		</div>
 	)
