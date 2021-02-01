af96a692be3632affe43acf2fd237b06b77f9407
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-02-01 21:52:33 +0900

    fix

--- news_app/src/components/ArticleCard.tsx
+++ news_app/src/components/ArticleCard.tsx
@@ -4,7 +4,9 @@ import "../styles/article_card.css"
 function ArticleCard() {
 	return (
 		<div className="card">
-			<div className="card--title">Title</div>
+			<div className="card--title">
+				<div className="card--title__name">ああああ</div>
+			</div>
 			<div className="card--contents">Contents</div>
 		</div>
 	)
